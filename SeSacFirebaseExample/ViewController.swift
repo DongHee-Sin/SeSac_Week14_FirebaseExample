1ca5d81acf417d4cc57def3e557f2e8561ed29c0
DongHee-Sin <voslr0601@naver.com>
2022-10-05 19:32:44 +0900

    Firebase) Crash, Analytics 테스트

--- SeSacFirebaseExample/ViewController.swift
+++ SeSacFirebaseExample/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import FirebaseAnalytics
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        Analytics.logEvent("share_image", parameters: [
+            "name": "고래밥",
+            "full_text": "안녕",
+        ])
+        
+        Analytics.setDefaultEventParameters([
+            "level_name": "Caverns01",
+            "level_difficulty": 4
+        ])
+        
+        
+        let button = UIButton(type: .roundedRect)
+        button.frame = CGRect(x: 20, y: 50, width: 100, height: 30)
+        button.setTitle("Test Crash", for: [])
+        button.addTarget(self, action: #selector(self.crashButtonTapped(_:)), for: .touchUpInside)
+        view.addSubview(button)
     }
 
-
+    
+    @IBAction func crashButtonTapped(_ sender: AnyObject) {
+        let numbers = [0]
+        let _ = numbers[1]
+    }
+    
 }
 
